47194835de27f009154dcefbbfe9f7def1c37f7c
Sriram <88694646+sriram-senthilkr@users.noreply.github.com>
2023-10-06 00:22:30 +0800

    add telegram bot notification

--- movie scraper.py
+++ movie scraper.py
@@ -1,64 +1,6 @@
-# import requests
-# from bs4 import BeautifulSoup
-# # import smtplib
-
-# def check_for_new_movies():
-#     url = 'https://carnivalcinemas.sg/#/Movies'
-
-#     # Send an HTTP GET request to the website
-#     response = requests.get(url)
-    
-
-#     # Check if the request was successful (status code 200)
-#     if response.status_code == 200:
-#         print("response 200 success")  # dubugging
-#         print(response.text)
-#         soup = BeautifulSoup(response.text, 'html.parser')
-
-#         # Extract movie listings from the webpage
-#         movie_listings = soup.find_all('div', class_='movies')  # Adjust class name as needed
-#         print(movie_listings)
-
-#         # Compare with previously stored movie data (you can store it in a file or database)
-#         # For simplicity, let's assume the previous_movies variable contains the old movie listings
-#         previous_movies = []
-
-#         # Check for new movies
-#         new_movies = [movie for movie in movie_listings if movie not in previous_movies]
-
-#         if new_movies:
-#             # If new movies are found, send an email notification
-#             # send_email_notification(new_movies)
-#             print("new_movies updates")
-
-# # Function to send an email notification
-# # def send_email_notification(new_movies):
-# #     # Email configuration
-# #     smtp_server = 'smtp.example.com'
-# #     smtp_port = 587
-# #     sender_email = 'your_email@example.com'
-# #     sender_password = 'your_email_password'
-# #     recipient_email = 'recipient@example.com'
-
-# #     # Compose the email message
-# #     subject = 'New Movies Added!'
-# #     message = '\n'.join([str(movie) for movie in new_movies])
-
-# #     # Connect to the SMTP server
-# #     server = smtplib.SMTP(smtp_server, smtp_port)
-# #     server.starttls()
-# #     server.login(sender_email, sender_password)
-
-# #     # Send the email
-# #     server.sendmail(sender_email, recipient_email, f'Subject: {subject}\n\n{message}')
-
-# #     # Disconnect from the server
-# #     server.quit()
-
-# # Example usage
-# check_for_new_movies()
-
 import time
+import requests
+import config
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -67,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
-### set-up
+### chrome driver set-up
 chrome_options = Options()
 chrome_options.add_argument('--headless') 
 chrome_options.add_argument('--disable-dev-shm-usage')
@@ -76,34 +18,18 @@ chrome_path = r'C:\Program Files (x86)\chromedriver.exe'
 chrome_service = ChromeService(chrome_path)
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-url = 'https://carnivalcinemas.sg/#/Movies'
+murl = 'https://carnivalcinemas.sg/#/Movies'
 
+### telegram bot set-up
+url = f"https://api.telegram.org/bot{config.telegram_token}"
+
+## variables
 current_movies = []
-previous_movies = []
+new_movies = []
 
-def check_for_changes(previous_movies, current_movies):
-    new_movies = [movie for movie in current_movies if movie not in previous_movies]
-    
-    # Notify if there are new movies
-    if new_movies:
-        with open('currentmovies.txt', 'w', encoding='utf-8') as file:
-            for movie in current_movies:
-                file.write(movie + '\n')
-        print("New movies detected:")
-        for movie in new_movies:
-            print(movie)
-    
-def retrieve_previous_movies(previous_movies):
-    try:
-        with open('currentmovies.txt', 'r', encoding='utf-8') as file:
-            previous_movies = file.readlines()
-            previous_movies = [movie.strip() for movie in previous_movies]
-    except FileNotFoundError:
-        previous_movies = []
-
-def scrape_movies(current_movies):
+def scrape_movies(new_movies):
     ### execution
-        driver.get(url)
+        driver.get(murl)
 
         # Wait for the element with class 'movies' to be present
         element = WebDriverWait(driver, 10).until(
@@ -118,23 +44,40 @@ def scrape_movies(current_movies):
             h2_element = movie.find('h2')
             if h2_element:
                 h2_content = h2_element.text
-                current_movies.append(h2_content)
+                new_movies.append(h2_content)
+
+def send_telegram_notification(message):
+    params = {"chat_id": config.chat_id, "text": message}
+    r = requests.get(url + "/sendMessage", params=params)
+
+def retrieve_current_movies():
+    try:
+        with open('currentmovies.txt', 'r', encoding='utf-8') as file:
+            movies_list = file.readlines()
+            movies_list = [movie.strip() for movie in movies_list]
+    except FileNotFoundError:
+        movies_list = []
+    return movies_list
+
+def check_for_changes(current_movies, new_movies):
+    changed_movies = [movie for movie in new_movies if movie not in current_movies]
+    
+    if changed_movies:
+        with open('currentmovies.txt', 'w', encoding='utf-8') as file:
+            for movie in new_movies:
+                file.write(movie + '\n')
+        message = "New movies detected:" + "\n" + "\n".join(changed_movies)
+        send_telegram_notification(message)
+        print(message)
+
 
 while True:
     try:
-        scrape_movies(current_movies)
+        scrape_movies(new_movies)
+        current_movies = retrieve_current_movies()
 
-        try:
-            with open('currentmovies.txt', 'r', encoding='utf-8') as file:
-                previous_movies = file.readlines()
-                previous_movies = [movie.strip() for movie in previous_movies]
-        except FileNotFoundError:
-            previous_movies = []
-        
-        # retrieve_previous_movies(previous_movies)
-
-        check_for_changes(previous_movies, current_movies)
-        current_movies = []
+        check_for_changes(current_movies, new_movies)
+        new_movies = []
     
         time.sleep(10)
 
